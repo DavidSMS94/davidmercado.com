be450d1ba6fc1e64ab345bf632d1cda9c53c1b74
DavidSMS94 <davidsms94@hotmail.com>
2017-07-09 02:26:23 -0700

    Initial scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name davidmercadocomApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the davidmercadocomApp
+ */
+angular.module('davidmercadocomApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name davidmercadocomApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the davidmercadocomApp
+ */
+angular.module('davidmercadocomApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
